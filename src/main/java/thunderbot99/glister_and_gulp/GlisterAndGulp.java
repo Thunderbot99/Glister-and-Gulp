ffb06e7ad16c814e8513531aa34e3c5225d7c506
Isaac <Thunderbot1994@gmail.com>
2025-06-26 15:01:12 +0800

    Start

---
+++ src/main/java/thunderbot99/glister_and_gulp/GlisterAndGulp.java
@@ -0,0 +1,15 @@
+package thunderbot99.glister_and_gulp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GlisterAndGulp implements ModInitializer {
+	public static final String MOD_ID = "glister-and-gulp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/thunderbot99/glister_and_gulp/GlisterAndGulpClient.java
@@ -0,0 +1,11 @@
+package thunderbot99.glister_and_gulp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlisterAndGulpClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
